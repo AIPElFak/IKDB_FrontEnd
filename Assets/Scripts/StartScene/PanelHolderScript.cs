fd179b1df956f824b5f387ce30d52c479dfe7b9f
Stefan Zivic <stefanzivic9@gmail.com>
2017-06-07 21:32:54 +0200

    Initiall
    Start scene added

---
+++ Assets/Scripts/StartScene/PanelHolderScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelHolderScript : MonoBehaviour {
+
+	public GameObject loginPanel;
+	public GameObject signupPanel;
+	// Use this for initialization
+	void Start () {
+		signupPanel.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/StartScene/RegisterButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegisterButtonScript : MonoBehaviour {
+
+	//public PanelHolderScript panelHolderScript;
+	public GameObject panelHolder;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void openRegisterForm() {
+		PanelHolderScript phs = panelHolder.GetComponent<PanelHolderScript> ();
+		phs.signupPanel.gameObject.SetActive (true);
+		phs.loginPanel.gameObject.SetActive (false);
+	}
+}
