9dbc688601f0fc670675330e6509ed64723f0db4
Stefan Zivic <stefanzivic9@gmail.com>
2017-06-10 22:29:37 +0200

    Delete node
    Delete node request and script

---
+++ Assets/Scripts/NodeInfoScene/DeleteNodeScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteNodeScript : MonoBehaviour {
+
+	public GameObject serverReq;
+	public DeleteNodeSuggestionRequestScript serverRequestScript;
+	// Use this for initialization
+	void Start()
+	{
+		serverRequestScript = serverReq.GetComponent<DeleteNodeSuggestionRequestScript>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	public void deleteNodeClick()
+	{
+
+		serverRequestScript.deleteNodeSuggestionRequest(DataHandler.SelectedNrds.node._id.ToString(), deleteNodeCallback);
+	}
+	public void deleteNodeCallback(string s)
+	{
+		if (s != null)
+		{
+			//EditorUtility.DisplayDialog("Error", "An error occurred", "Ok");
+			Debug.Log("An error occurred");
+		}
+		else
+		{
+			//EditorUtility.DisplayDialog("Success", "Success", "Ok");
+			Debug.Log("Node delete suggestion send successfully");
+		}
+	}
+}
---
+++ Assets/Scripts/NodeInfoScene/DeleteNodeSuggestionRequestScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeleteNodeSuggestionRequestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void deleteNodeSuggestionRequest(String node_id,
+		Action<String> deleteNodeSuggestionCallback)
+	{
+		string data = "{\"author\":\"" + DataHandler.UserId + "\",\"suggestion_type\":\"DELETE\",\"node_id\":\"" +
+			node_id + "\"}";
+
+		Debug.Log(data);
+		Dictionary<string, string> header = new Dictionary<string, string>();
+		header.Add("Content-Type", "application/json");
+		header.Add("token", DataHandler.Token);
+		var byteData = System.Text.UTF8Encoding.UTF8.GetBytes(data);
+		WWW www = new WWW(DataHandler.serverIP + ": 3000 /secure/createnodesuggestion", byteData, header);
+		StartCoroutine(waitForDeleteNodeSuggestionRequest(www, deleteNodeSuggestionCallback));
+	}
+
+	IEnumerator waitForDeleteNodeSuggestionRequest(WWW www, Action<String> deleteNodeSuggestionCallback)
+	{
+		yield return www;
+		deleteNodeSuggestionCallback(www.error);
+	}
+
+}
