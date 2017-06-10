d29f0097bd7e9820bc39f0adfbf9731d441f9529
Stefan Zivic <stefanzivic9@gmail.com>
2017-06-10 23:10:24 +0200

    Node votes
    Vote for node req and scripts

---
+++ Assets/Scripts/NodeInfoScene/VoteNodeServerRequests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class VoteNodeServerRequests : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void nodeVoteRequest(String node_id, String vote ,
+		Action<String> voteNodeCallback)
+	{
+		string data = "{\"user_id\":\"" + DataHandler.UserId + "\",\"vote\":\"" + vote + "\",\"node_id\":\"" +
+			node_id + "\"}";
+
+		//   Debug.Log(data);
+		Dictionary<string, string> header = new Dictionary<string, string>();
+		header.Add("Content-Type", "application/json");
+		header.Add("token", DataHandler.Token);
+		var byteData = System.Text.UTF8Encoding.UTF8.GetBytes(data);
+		WWW www = new WWW(DataHandler.serverIP + ": 3000 /secure/voteonnode", byteData, header);
+		StartCoroutine(waitForVoteNodeRequest(www, voteNodeCallback));
+	}
+
+	IEnumerator waitForVoteNodeRequest(WWW www, Action<String> voteNodeCallback)
+	{
+		yield return www;
+		voteNodeCallback(www.error);
+	}
+
+}
---
+++ Assets/Scripts/NodeInfoScene/VoteOnNodeScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoteOnNodeScript : MonoBehaviour {
+	
+	GameObject serverRequest;
+	VoteNodeServerRequests voteNodeServerRequest;
+	// Use this for initialization
+	void Start () {
+		voteNodeServerRequest = serverRequest.GetComponent<VoteNodeServerRequests> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void voteForNodeClick()
+	{
+
+		voteNodeServerRequest.nodeVoteRequest(DataHandler.SelectedNrds.node._id.ToString(), "POSITIVE", voteForNodeCallback);
+	}
+
+	public void voteForNodeCallback(string s)
+	{
+		if (s != null)
+		{
+			//EditorUtility.DisplayDialog("Error", "An error occurred", "Ok");
+			Debug.Log("Error");
+
+		}
+		else
+		{
+			//EditorUtility.DisplayDialog("Success", "Success", "Ok");
+			Debug.Log("Success");
+		}
+	}
+	public void voteAgainstNodeClick()
+	{
+		voteNodeServerRequest.nodeVoteRequest(DataHandler.SelectedNrds.node._id.ToString(), "NEGATIVE", voteAgainstNodeCallback);
+	}
+
+	public void voteAgainstNodeCallback(string s)
+	{
+		if (s != null)
+		{
+			//EditorUtility.DisplayDialog("Error", "An error occurred", "Ok");
+			Debug.Log("Error");
+
+		}
+		else
+		{
+			//EditorUtility.DisplayDialog("Success", "Success", "Ok");
+			Debug.Log("Success");
+		}
+	}
+}
